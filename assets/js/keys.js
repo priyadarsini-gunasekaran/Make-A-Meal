9d00f70a14c9d03565f4580d380e6eea3ff4ae76
sabina akinbusuyi <akinbusuyi@gmail.com>
2023-02-08 22:31:58 +0000

    adjusted keys

---
+++ assets/js/keys.js
@@ -0,0 +1,16 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'Accept-Encoding': 'application/gzip',
+		'X-RapidAPI-Key': '4934e44338msh7ed89d55768b267p1b6b52jsne69c880e1416',
+		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+	}
+};
+
+const options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4934e44338msh7ed89d55768b267p1b6b52jsne69c880e1416',
+		'X-RapidAPI-Host': 'themealdb.p.rapidapi.com'
+	}
+};
